68e53a01ca4a5ac6f4f01218f484039e47055c7e
pepmartinez <pep.martinez@gmail.com>
2023-04-16 12:15:18 +0200

    change to MIT license

--- examples/overflow.js
+++ examples/overflow.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 
 var TR =   require('../').Transport;
 
-var logger = winston.createLogger({
+var logger1 = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.splat(),
@@ -25,7 +25,32 @@ var logger = winston.createLogger({
   ]
 });
 
+var logger2 = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.simple()
+  ),
+  transports: [
+    new TR ({
+      level: 'info',
+      host: '127.0.0.1',
+      port: 5558,
+      timestamp: true,
+      class_id: 'example-2',
+      node_id: 'l1',
+      extra: {
+        x1: 'qwertyuiop',
+        x2: 666
+      }
+    })
+  ]
+});
+
+
+let c = 0;
 setInterval (function () {
-  logger.info ('logging... %s %d %j', 'ggg', 666, {a:1,b:2}, {extra:0});
+  logger1.info ('logging... %s %d %j', 'ggg', c++, {a:1,b:2}, {extra:0});
+  logger2.info ('logging... %s %d %j', 'ggg', c++, {a:1,b:2}, {extra:0});
 }, 100);
 
